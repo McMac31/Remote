05a4cd389db61a4541b773fe1022a389119f5074
unknown <miguel317.angelps@gmail.com>
2025-02-12 12:28:33 +0100

    Excluyendo javas

--- archivo2.java
+++
@@ -1,5 +0,0 @@
-public class archivo2{
-	public static void main (String [] args){
-	System.out.printIn("Hola2")
-	}
-}
--- archivo3.java
+++
@@ -1,5 +0,0 @@
-public class archivo3{
-	public static void main(String [] args){
-	System.out.printIn("Hola3")
-	}
-}
